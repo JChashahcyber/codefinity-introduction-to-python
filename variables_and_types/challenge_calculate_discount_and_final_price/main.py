c24962f2afa4782b1fdb098be82ede8aac9c0b2b
JChashahcyber <>
2026-03-19 05:56:29 +0000

    Challenge: Calculate Discount and Final Price

---
+++ variables_and_types/challenge_calculate_discount_and_final_price/main.py
@@ -0,0 +1,6 @@
+original_price = 250.0
+discount_rate = 0.2
+discount_amount = (original_price * discount_rate)
+final_price = (original_price - discount_amount)
+print(discount_amount)
+print(final_price)
