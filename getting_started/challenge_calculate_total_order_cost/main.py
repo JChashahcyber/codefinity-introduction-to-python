657849ae8451f31a71185e8fbd17350064234a02
JChashahcyber <>
2026-03-19 05:32:58 +0000

    Challenge: Calculate Total Order Cost

---
+++ getting_started/challenge_calculate_total_order_cost/main.py
@@ -0,0 +1,14 @@
+# Calculate the cost of three shirts
+shirt_cost = 3 * 20
+
+# Calculate the cost of two hats
+hat_cost = 2 * 15
+
+# Apply coupon
+subtotal = shirt_cost + hat_cost - 10
+
+# Add 8% tax
+total = subtotal * (1 + 8/100)
+
+# Print final order cost
+print(total)
